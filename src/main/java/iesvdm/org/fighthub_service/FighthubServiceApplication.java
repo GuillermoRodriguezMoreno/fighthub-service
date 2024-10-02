fdb2d69b5e4cfe1041d1d15d475e75cdfdc1a89e
guille <guille@ejemplo.es>
2024-10-02 19:09:07 +0200

    Firs commit

---
+++ src/main/java/iesvdm/org/fighthub_service/FighthubServiceApplication.java
@@ -0,0 +1,13 @@
+package iesvdm.org.fighthub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FighthubServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FighthubServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iesvdm/org/fighthub_service/FighthubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package iesvdm.org.fighthub_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FighthubServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
